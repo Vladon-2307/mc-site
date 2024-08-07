58837a4a0a9c8f30f888406fd27475fcb7d154c0
Vladon-2307 <79317427+Vladon-2307@users.noreply.github.com>
2024-08-07 12:35:56 +0300

    добавлен бан-лист

--- src/app/pages/ban-list/ban-list.component.ts
+++ src/app/pages/ban-list/ban-list.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DatePipe} from "@angular/common";
+
+export interface banItem{
+  banNik: string,
+  banedNik: string,
+  ban: Date,
+  unban: Date,
+  reason: string
+}
 
 @Component({
   selector: 'app-ban-list',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './ban-list.component.html',
   styleUrl: './ban-list.component.scss'
 })
 export class BanListComponent {
 
+  banList: banItem[] = [
+    {
+      banNik: 'dasdsadasdasdsadasd',
+      banedNik: 'dasdsadasd',
+      ban: new Date(2024, 8, 7, 13, 30),
+      unban: new Date(),
+      reason: '1.2 fsdfsdf'
+    },
+    {
+      banNik: 'dasdsadasd',
+      banedNik: 'dasdsadasd',
+      ban: new Date(),
+      unban: new Date(),
+      reason: '1.2 fsdfsdf'
+    },
+    {
+      banNik: 'dasdsadasd',
+      banedNik: 'dasdsadasd',
+      ban: new Date(),
+      unban: new Date(),
+      reason: '1.2 fsdfsdf'
+    },
+  ]
+
+  constructor() {
+    console.log(Date.now())
+  }
 }
